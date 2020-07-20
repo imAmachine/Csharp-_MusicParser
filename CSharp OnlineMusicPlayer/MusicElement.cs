5589afc0323e1fc17b8f8bc4b1671a4a2152e95f
imAmachine <vlad.makarin22@gmail.com>
2020-07-20 15:55:40 +0300

    Added methods for getting webpage. Added class including music information

---
+++ CSharp OnlineMusicPlayer/MusicElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OnlineMusicPlayer
+{
+    public class MusicElement
+    {
+        public string URL { get; set; }
+        public string MusicName { get; set; }
+        public DateTime MusicDuration { get; set; }
+
+        /// <summary>
+        /// Данный класс позволяет хранить всю необходимую информацию о музыкальном треке
+        /// </summary>
+        /// <param name="url">Прямая ссылка на музыкальный трек</param>
+        /// <param name="musicName">Название музыкального трека</param>
+        /// <param name="musicDuration">Продолжительность музыкального трека</param>
+        public MusicElement(string url, string musicName, DateTime musicDuration)
+        {
+            URL = url;
+            MusicName = musicName;
+            MusicDuration = musicDuration;
+        }
+    }
+}
---
+++ CSharp OnlineMusicPlayer/WWW.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+
+namespace CSharp_OnlineMusicPlayer
+{
+    public static class WWW
+    {
+        public static string GetPage(string link)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                    return sr.ReadToEnd();
+
+            return string.Empty;
+        }
+
+        //public static List<MusicElement> GetMusicList(string webPage)
+        //{
+
+        //}
+    }
+}
