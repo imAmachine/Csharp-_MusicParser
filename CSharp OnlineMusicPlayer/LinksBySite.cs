e7cb212eb8fa50eef6347c2e986821ffc372468e
V.Coder <vlad.makarin22@gmail.com>
2020-10-30 10:22:36 +0300

    removed useless lines of code

--- CSharp OnlineMusicPlayer/LinksBySite.cs
+++ CSharp OnlineMusicPlayer/LinksBySite.cs
@@ -19,14 +19,5 @@ namespace CSharp_OnlineMusicPlayer
                     break;
             }
         }
-
-        //public static void ZaycevNetMusicLinks(List<MusicElement> music)
-        //{
-        //    music.ForEach(x =>
-        //    {
-        //        List<Match> matches = WWW.GetMatches(@"""url"":""(.*)""", x.URL.Insert(0, @"https://zaycev.net"));
-        //        x.URL = matches.First().Groups[1].Value;
-        //    });
-        //}
     }
 }
